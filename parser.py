a7c7837c1cd463710d51542e3245898071dc10fb
eqtstv <38236287+eqtstv@users.noreply.github.com>
2021-03-31 21:19:05 +0200

    Add ArgumentParser

--- parser.py
+++ parser.py
@@ -1,18 +1,44 @@
 import argparse
 import regex as re
-import sys
 
 
-class Parser:
-    def __init__(self, parser):
-        self.parser = parser
+def get_argparser():
+    parser = argparse.ArgumentParser(
+        description="Txt file parser.\
+            Convert indentation (tabs to spaces and vice versa)."
+    )
 
-    def tabs_to_spaces(self, no_spaces):
+    parser.add_argument(
+        "-f",
+        "--from",
+        type=str,
+        default=False,
+        choices=["tabs", "spaces"],
+        help="Convert from: ('tabs' or 'spaces')",
+    )
+    parser.add_argument(
+        "-r",
+        "--replace",
+        action="store_true",
+        help="Replace original file",
+    )
+    parser.add_argument(
+        "-t",
+        "--tab-chars",
+        type=int,
+        default=4,
+        help="Number of spaces for one tab",
+    )
+
+    return vars(parser.parse_args())
+
+
+class TxtParser:
+    def convert_tabs_to_spaces(self, no_spaces):
         fin = open("is_prime.txt", "r")
         fout = open("output.txt", "w")
         regex_search_term = r"\G\t"
         regex_replacement = " " * no_spaces
-        print(regex_search_term)
         for line in fin:
             text_after = re.sub(regex_search_term, regex_replacement, line)
             fout.write(text_after)
@@ -20,14 +46,12 @@ class Parser:
         fin.close()
         fout.close()
 
-    def spaces_to_tabs(self, no_spaces):
+    def convert_spaces_to_tabs(self, no_spaces):
         fin = open("is_prime.txt", "r")
         fout = open("output.txt", "w")
-        regex_search_term = r"\G\s{4}"
+        regex_search_term = f"\\G\\s{ {no_spaces} }"
         regex_replacement = "\t"
-        print(regex_search_term)
         for line in fin:
-            print(re.findall(regex_search_term, line))
             text_after = re.sub(regex_search_term, regex_replacement, line)
             fout.write(text_after)
 
@@ -36,13 +60,13 @@ class Parser:
 
 
 if __name__ == "__main__":
-    if "-f" in sys.argv:
-        print("-f in")
-    if "-r" in sys.argv:
-        print("-r in")
-    if "-t" in sys.argv:
-        print("-t in")
-    print(sys.argv)
+    args = get_argparser()
 
-    parser = Parser("parser")
-    parser.spaces_to_tabs(4)
+    if not args["from"]:
+        print("None")
+    elif args["from"] == "tabs":
+        txt_parser = TxtParser()
+        txt_parser.convert_tabs_to_spaces(args["tab_chars"])
+    elif args["from"] == "spaces":
+        txt_parser = TxtParser()
+        txt_parser.convert_spaces_to_tabs(args["tab_chars"])
