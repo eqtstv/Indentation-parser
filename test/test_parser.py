98f2e06373719534b1eb18aeee15db5897386807
eqtstv <38236287+eqtstv@users.noreply.github.com>
2021-04-02 14:08:44 +0200

    Add tests for parse_txt_file

--- test/test_parser.py
+++ test/test_parser.py
@@ -1,3 +1,4 @@
+import glob
 import os
 import unittest
 
@@ -121,10 +122,97 @@ class TestTxtParser(unittest.TestCase):
 
 class TestParseTxtFile(unittest.TestCase):
     def test_check_main_indentation_type(self):
-        result = parse_txt_file("is_prime.txt")
+        # GIVEN test txt file
+        filename = "is_prime.txt"
+        # WHEN parse_txt_file() is ran with test file
+        result = parse_txt_file(filename)
+
+        # THEN it sould return proper message
         self.assertEqual(
             result,
             "File has:\n    rows indented with tabs: 2\
                 \n    rows indented with spaces: 7\
                 \n\nThe file has mainly spaces as indentation",
         )
+
+    def test_from_tabs_to_spaces(self):
+        # GIVEN test txt file
+        filename = "is_prime.txt"
+
+        # AND other test files are deleted
+        for filename in glob.glob(f"{filename[:-4]}_parsed_*"):
+            os.remove(filename)
+
+        # WHEN parse_txt_file is ran with that file, and "tabs" argument
+        result = parse_txt_file("is_prime.txt", "tabs")
+
+        # THEN is sould modify 2 rows and create is_prime_parsed_0.txt
+        self.assertEqual(
+            result,
+            "\nRows modified: 2\
+                \nCreated file: is_prime_parsed_0.txt",
+        )
+
+    def test_from_spaces_to_tabs(self):
+        # GIVEN test txt file
+        filename = "is_prime.txt"
+
+        # AND other test files are deleted
+        for filename in glob.glob(f"{filename[:-4]}_parsed_*"):
+            os.remove(filename)
+
+        # WHEN parse_txt_file is ran with that file, and "spaces" argument
+        result = parse_txt_file("is_prime.txt", "spaces")
+
+        # THEN is sould modify 7 rows and create is_prime_parsed_0.txt
+        self.assertEqual(
+            result,
+            "\nRows modified: 7\
+                \nCreated file: is_prime_parsed_0.txt",
+        )
+
+    def test_from_tabs_to_spaces_inplace(self):
+        # GIVEN test txt file
+        filename = "is_prime.txt"
+        test_filename = "test_file.txt"
+
+        # AND copy test file
+        with open(test_filename, "w+") as outfile, open(filename, "r") as infile:
+            for line in infile:
+                text_after = line
+                outfile.write(text_after)
+
+        # WHEN parse_txt_file is ran with that file, "tabs" argument and replace=True
+        result = parse_txt_file(test_filename, "tabs", True)
+
+        # THEN is sould modify 2 rows inplace
+        self.assertEqual(
+            result,
+            "\nRows modified: 2\
+                \nParsed file inplace",
+        )
+
+        os.remove(test_filename)
+
+    def test_from_spaces_to_tabs_inplace(self):
+        # GIVEN test txt file
+        filename = "is_prime.txt"
+        test_filename = "test_file.txt"
+
+        # AND copy test file
+        with open(test_filename, "w+") as outfile, open(filename, "r") as infile:
+            for line in infile:
+                text_after = line
+                outfile.write(text_after)
+
+        # WHEN parse_txt_file is ran with that file, "spaces" argument and replace=True
+        result = parse_txt_file(test_filename, "spaces", True)
+
+        # THEN is sould modify 7 rows inplace
+        self.assertEqual(
+            result,
+            "\nRows modified: 7\
+                \nParsed file inplace",
+        )
+
+        os.remove(test_filename)
